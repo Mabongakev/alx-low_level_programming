6163c0f399b51dfd3b623aa5eb4ad9f30ba5d268
Mabongakev <kelvin.mabonga92@gmail.com>
2023-10-05 14:30:16 -0700

    use of printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /*
- * main - use the function printf
+ * main - Use of printf
  *
  * Return: 0
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art");
+	printf("with proper grammar, but the outcome is a piece of art\n");
 	return (0);
 }

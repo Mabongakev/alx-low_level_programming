7008dabc6c2c4b7236a53c5236f0db29d2ef838f
Mabongakev <kelvin.mabonga92@gmail.com>
2023-10-06 15:21:14 -0700

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+    	for (k = 'a' ; k <= 'z' ; k++)
+			putchar(k)
+		putchar('\n')
+    	return (0);
+}
+

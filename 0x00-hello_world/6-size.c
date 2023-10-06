c1750f871e5c690f421fc3b187a6efda21d35b30
Mabongakev <kelvin.mabonga92@gmail.com>
2023-10-06 08:50:49 -0700

    bytes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,5 +11,6 @@ int main(void)
 	printf("Size of a int: %lh byte(s)", sizeof(int));
 	printf("Size of a long int: %lh byte(s)", sizeof(long int));
 	printf("Size of a long long int: %lh byte(s)", sizeof(long long int));
+	printf("Size of a float: %lh byte(s)", sizeof(float);
     	return (0);
 }

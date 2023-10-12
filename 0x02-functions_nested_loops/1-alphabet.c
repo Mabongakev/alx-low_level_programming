27c00b4f48ea1a1d90099c419ff666c5f1786449
Mabongakev <kelvin.mabonga92@gmail.com>
2023-10-12 07:17:35 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print alphabet - function to print alphabets
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char k;
+
+	for (k = 'a' ; k <= 'z' ; k++)
+		_putchar(k);
+	_putchar('\n');
+}

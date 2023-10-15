492dd701ff0ea633ed8a77f0ebbdc640708d2862
Mabongakev <kelvin.mabonga92@gmail.com>
2023-10-15 04:34:30 -0700

    print a straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_line - draws a straight line
  * @n: parameter to be checked
@@ -8,7 +9,7 @@
 void print_line(int n)
 {
 	if (n <= 0)
-		putchar('\n');
+		_putchar('\n');
 	else
 	{
 		int k;

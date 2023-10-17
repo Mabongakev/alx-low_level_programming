65d6c9121bbeec4b22a6382d24f1817c545525e2
Mabongakev <kelvin.mabonga92@gmail.com>
2023-10-17 03:02:14 -0700

    a function that prints half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts_half - a function that prints a string, in reverse
+ * @str: parameter to be checked
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int k, l, count = 0;
+
+	for (k = 0 ; str[k] != '\0' ; k++)
+		count++;
+	l = (count - 1) / 2;
+	for (k = l + k ; str[k] != '\0' ; k++)
+		_putchar(str[k]);
+	_putchar('\n');
+}

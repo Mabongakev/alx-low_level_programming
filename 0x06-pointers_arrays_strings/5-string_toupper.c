4d2e8699bc0aaa34e6bdedde8baf6689735ef8cd
Mabongakev <kelvin.mabonga92@gmail.com>
2023-10-19 15:51:25 -0700

    converting to upper case

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,11 +9,11 @@ char *string_toupper(char *n)
 	int k;
 
 	k = 0;
-	while (n[k])
+	while (n[k] != '\0')
 	{
 		if (n[k] >= 'a' && n[k] <= 'z')
 			n[k] -= 32;
 		k++;
 	}
-	return (0);
+	return (n);
 }

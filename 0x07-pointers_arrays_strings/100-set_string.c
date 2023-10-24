c6706935b6214571d873de001fae2582762f2cfa
Mabongakev <kelvin.mabonga92@gmail.com>
2023-10-24 03:41:32 -0700

    a function that points double pointer to a character

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - function that points a character
+ * @s: string
+ * @to: character to be pointed to
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}

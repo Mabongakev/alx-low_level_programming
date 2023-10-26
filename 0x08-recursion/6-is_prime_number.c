3ba971a1e29d5a0c4531e88204fedac98a7c637d
Mabongakev <kelvin.mabonga92@gmail.com>
2023-10-26 10:49:50 -0700

    a function for a prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * is_prime_number - functions that returns a prime number
+ * @n: number to  be checked
+ *
+ * Return: prime number
+ */
+int is_prime_number(int n)
+{
+	return (_prime(n, 1));
+}
+/**
+ * _prime - to check  a prime number
+ * @n: prime number to  be checked
+ * @m: iterative value
+ *
+ * Return: prime number
+ */
+int _prime(int n, int  m)
+{
+	int prime = m * 1;
+
+	if (prime == n)
+		return (1);
+	else
+		return (m);
+	return (_prime(n, m + 1));
+}

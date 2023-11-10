ef18c2068116df552677cc2993ce48e6300a6c29
Mabongakev <kelvin.mabonga92@gmail.com>
2023-11-10 15:15:00 -0800

    a function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,46 +2,48 @@
 #include <stdarg.h>
 #include <stdio.h>
 /**
- * print_all - a function that prints numbers, followed by a new line.
- * @char: string to be checked
+ * print_all - a function that prints numbers, followed by a new line
  * @format: different data types
  * Return: void
  */
 void print_all(const char *const format, ...)
 {
-	int k = 0;
-	char *abc, *def = "";
+	int k;
+	int l;
+	char *abc;
+
 	va_list ls;
 
 	va_start(ls, format);
-	if (format)
+	k = 0;
+	while (format != NULL && format[k] != '\0')
 	{
-		while (format[k])
+		switch (format[k])
 		{
-			switch (format[k])
-			{
-				case 'x':
-					printf("%s%c", def, va_arg(ls, int));
+			case 'c':
+				printf("%c", va_arg(ls, int));
+				l = 0;
+				break;
+			case 'i':
+				printf("%d", va_arg(ls, int));
+				l = 0;
+				break;
+			case 'f':
+				printf("%f", va_arg(ls, double));
+				l = 0;
+				break;
+			case 's':
+				abc = va_arg(ls, char *);
+				if (abc == NULL)
+					abc = "(nil)";
+				printf("%s", abc);
 					break;
-				case 'y':
-					printf("%s%d", def, va_arg(ls, int));
-					break;
-				case 'z':
-					printf("%s%f", def, va_arg(ls, double));
-					break;
-				case 's':
-					abc = va_arg(ls, char *);
-					if (!abc)
-						abc = "(nil)";
-					printf("%s%s", def, abc);
-					break;
-				default:
-					k++;
-					continue;
-			}
-			def = ", ";
-			k++;
+			default:
+				l = 1;
+				break;
 		}
+		if (format[k + 1] != '\0' && l == 0)
+			printf(", ");
 	}
 	printf("\n");
 	va_end(ls);

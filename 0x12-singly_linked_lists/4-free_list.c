8ad8bb0b4fc43b4808a878786eb555ed5aeb33a6
Mabongakev <kelvin.mabonga92@gmail.com>
2023-11-17 08:52:32 -0800

    Free the list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_list - to free nodes on the list
+ * @head: first node
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *current_node;
+
+	while ((current_node = head) != NULL)
+	{
+		head = head->next;
+		free(current_node->str);
+		free(current_node);
+	}
+
+}

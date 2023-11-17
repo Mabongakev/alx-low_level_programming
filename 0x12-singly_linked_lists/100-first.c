69674fde2e8224b95f54bdf0998ae5f7a06fd7e5
Mabongakev <kelvin.mabonga92@gmail.com>
2023-11-17 09:56:43 -0800

    function that prints

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void print_(void);
+/**
+ * print_ - print the 1st sentence
+ * Return: void
+ */
+void print_(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+
+}

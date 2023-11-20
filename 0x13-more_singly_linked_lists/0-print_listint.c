9273aa93d17cfe78fa0552de0fcc98ca579184c1
Mabongakev <kelvin.mabonga92@gmail.com>
2023-11-20 12:48:38 -0800

    a function that prints all elements of the list3

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,13 +10,12 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t count = 0;
+	size_t count;
 
 	if (h == NULL)
-		printf("Empty list");
-	while (h != NULL)
+		return (0);
+	for (count = 0; h != NULL; count++)
 	{
-		count++;
 		printf("%d\n", h->n);
 		h = h->next;
 	}

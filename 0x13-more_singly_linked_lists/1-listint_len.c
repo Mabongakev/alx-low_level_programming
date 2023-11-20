782b3876c509e015e147ab144b75fd3919ef8f5d
Mabongakev <kelvin.mabonga92@gmail.com>
2023-11-20 12:59:37 -0800

    a fucntion that returns all the elments

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * listint_len - a function that returns elements of the list
+ * @h: head node
+ * Return: count
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	if (h == NULL)
+		return (0);
+	while (h != NULL)
+		count++;
+	return (count);
+}

56787de867c25d84660009d0fd70eb78692f7cbf
Mabongakev <kelvin.mabonga92@gmail.com>
2023-11-24 08:49:35 -0800

    a function that converts an int into binary

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * binary_to_uint - a function that converts a binary number
+ * @b: pointer to as string
+ * Return: void
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int k;
+	unsigned int n;
+
+	if (b != NULL)
+		return (0);
+	for (k = 0; b[k] != '\0'; k++)
+	{
+		if (b[k] != '0' && b[k] != '1')
+			return (0);
+	}
+	for (k = 0; b[k] != '\0'; k++)
+	{
+		n <<= 1;
+		if (b[k] == '1')
+			n += 1;
+	}
+	return (n);
+}

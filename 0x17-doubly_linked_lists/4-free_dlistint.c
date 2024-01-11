34e622ac2a932e75f419c193fa0b90b7212812fb
Mabongakev <kelvin.mabonga92@gmail.com>
2024-01-11 07:26:29 -0800

    a function that frees a dlistint_t list

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -10,15 +10,10 @@
  */
 void free_dlistint(dlistint_t *head)
 {
-	dlistint_t *temp;
-
-	if (head != NULL)
-		while (head->prev != NULL)
-			head = head->prev;
-	temp = head;
-	while (temp != NULL)
-	{
+	if (head == NULL)
+		return (NULL);
+	while (head->next != NULL)
 		head = head->next;
-		free(temp);
-	}
+	free(head->prev);
+	free(head);
 }

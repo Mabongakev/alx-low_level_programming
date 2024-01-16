b0c490b09a1d821e74c4da15980531ca5c6800f0
Mabongakev <kelvin.mabonga92@gmail.com>
2024-01-16 01:46:28 -0800

    dynamic library that contains C functions that can be called from Python

---
+++ 0x18-dynamic_libraries/calc.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+/**
+ * add - a sum funct
+ * @a: first element
+ * @b: second element
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - a subtraction funct
+ * @a: first element
+ * @b: second element
+ * Return: subtraction
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - a multiplication funct
+ * @a: first element
+ * @b: second element
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - a division funct
+ * @a: first element
+ * @b: second element
+ * Return: division
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - a reminder funct
+ * @a: first element
+ * @b: second element
+ * Return: a % b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}

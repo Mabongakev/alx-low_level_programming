f80064eb0cc056e8fa9840ae8ccce565436bff20
Mabongakev <kelvin.mabonga92@gmail.com>
2024-02-05 12:54:08 -0800

    a function that creates a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,7 +17,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_t->size = size;
 	new_t->array = malloc(sizeof(hash_node_t *) * size);
 	if (new_t->array == NULL)
+	{
 		return (NULL);
+		free(new_t);
+	}
 	for (k = 0; k < size; k++)
 		new_t->array[k] = NULL;
 
